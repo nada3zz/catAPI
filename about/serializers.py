4e6d9a13854ee2b18dba15cf138261c7b11fa5fe
Nada Ezzdin <nadaeezzdin@gmail.com>
2021-07-13 01:42:35 +0200

    Remove extra fields from about.serializers

--- about/serializers.py
+++ about/serializers.py
@@ -15,11 +15,11 @@ class HistorySerializer(serializers.ModelSerializer):
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
-        fields = ('id', 'title_en','title_ar', 'body_en','body_ar', 'image', 'publish', 'updated')
+        fields = ('id', 'title', 'body', 'image', 'publish', 'updated')
 
 
 class AboutSerializer(serializers.ModelSerializer):
     class Meta:
         model = AboutTeam
-        fields = ('id', 'title_en','title_ar', 'body_en', 'body_ar')
+        fields = ('id', 'title','body')
 
