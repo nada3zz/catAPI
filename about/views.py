7f0cf869f20e04ff511bbe14494fd3439ea38761
TAHER-MOSTAFA <taher.mostafa808@gmail.com>
2021-07-28 19:45:51 +0200

    Add Home ViewSet

--- about/views.py
+++ about/views.py
@@ -1,21 +1,27 @@
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from .serializers import AboutSerializer, HistorySerializer, NewsSerializer, HomeSerializer
 from .models import AboutTeam, History, News, Home
 from rest_framework import permissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
+class News(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+class Home(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
+    queryset = Home.objects.all()
+    serializer_class = HomeSerializer
 
 class NewsAndHomeViewset(viewsets.ReadOnlyModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
     serializer_class_news = NewsSerializer
     serializer_class_home = HomeSerializer 
-    def get_queryset_News(self):
-        return News.objects.all()
-
-    def get_queryset_Home(self):
-        return Home.objects.all()
-
+    
     def list(self, request, *args, **kwargs):
         news = self.serializer_class_news(self.get_queryset_News(), many=True)
         home = self.serializer_class_home(self.get_queryset_Home(), many=True)
@@ -26,23 +32,6 @@ class NewsAndHomeViewset(viewsets.ReadOnlyModelViewSet):
         })
     
     
-    def retrieve(self, request, pk=None ,*args, **kwargs):
-        queryset =News.objects.all()
-        instance = get_object_or_404(queryset, pk=pk)
-        #title = self.kwargs.get('id')
-        serializer = NewsSerializer(instance)
-       
-        return Response(serializer.data)
-   
-"""
-    def retrieve(self, request, pk=None, *args, **kwargs):
-        queryset =Home.objects.all()
-        instance = get_object_or_404(queryset, pk=pk)
-        serializer = HomeSerializer(instance)
-
-        return Response(serializer.data)
-"""
-
 
 class AboutViewset(viewsets.ReadOnlyModelViewSet): 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
